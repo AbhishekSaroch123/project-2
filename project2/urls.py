3bc02148d1c806a918514ff88e8b20ade63742e0
AbhishekSaroch123 <abhishek@prodsmiths>
2023-09-19 19:53:23 +0530

    Created Forms

--- project2/urls.py
+++ project2/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from . import views
 app_name = "project2"
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path('user/<int:pk>/', views.UserInformationView.as_view(), name='user-information'),
+    path('create_user/', views.create_user, name='create-user'),
 ]
--- project2/views.py
+++ project2/views.py
@@ -1,13 +1,22 @@
 from django.http import HttpResponse
 from django.views import generic
 from .models import *
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render,get_object_or_404
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+from django.shortcuts import render, redirect
+from .models import User
+from .forms import UserCreationForm
+
+
+
 class IndexView(generic.ListView):
     template_name = "project2/index.html"
     context_object_name = "latest_users_list"
 
     def get_queryset(self):
-    
         return User.objects.all()
     
 class UserInformationView(generic.DetailView):
@@ -18,7 +27,6 @@ class UserInformationView(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.get_object()  # Get the user object
-
         # Retrieve data from related models and add it to the context
         context['analytics'] = Analytics.objects.filter(user=user).first()
         context['resources'] = Resources.objects.filter(user=user)
@@ -29,4 +37,30 @@ class UserInformationView(generic.DetailView):
         context['people_you_may_know'] = PeopleYouMayKnow.objects.filter(user=user)
 
         return context
+
+
+    
+# def create_user(request):
+#          if request.method == 'POST':
+#           form = UserCreationForm(request.POST)
+#           if form.is_valid():
+#            form.save()  # Save the user data to the database
+#            return redirect('project2:index')  # Redirect to the index page after user creation
+#          else:
+#           form = UserCreationForm()
+
+#          return render(request, 'project2/create_user.html', {'form': form})
+
+def create_user(request):
+         if request.method == 'POST':
+          form = UserCreationForm(request.POST)
+          if form.is_valid():
+           form.save()  # Save the user data to the database
+           return redirect('project2:index')  # Redirect to the index page after user creation
+         else:
+          form = UserCreationForm()
+
+         return render(request, 'project2/create_user.html', {'form': form})
+
+
 
