270ef25729b9391189255c265e20182749d972bb
AbhishekSaroch123 <abhishek@prodsmiths>
2023-09-22 19:04:45 +0530

    created a test case

--- project2/tests.py
+++ project2/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from .models import User
 
-# Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self):
+        # Create some sample user data for testing
+        User.objects.create(name="Test User 1", role="Developer", location="City 1", connections=100, profile_language="English", public_profile_url="https://example.com/testuser1")
+        User.objects.create(name="Test User 2", role="Designer", location="City 2", connections=50, profile_language="Spanish", public_profile_url="https://example.com/testuser2")
+
+    def test_user_creation(self):
+        # Test user creation via API
+        url = reverse('project2:create_user_api')
+        data = {
+            "name": "New User",
+            "role": "Tester",
+            "location": "City 3",
+            "connections": 10,
+            "profile_language": "French",
+            "public_profile_url": "https://example.com/newuser"
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+ 
--- project2/urls.py
+++ project2/urls.py
@@ -3,11 +3,6 @@ from . import views
 app_name = "project2"
 
 urlpatterns = [
-    # path("", views.IndexView.as_view(), name="index"),
-    # path('user/<int:pk>/', views.UserInformationView.as_view(), name='user-information'),
-    # path('create_user/', views.create_user, name='create-user'),
-    #  path('create_user/', views.create_user, name='create_user'),
-    # path("",views.index,name="index"),
      path('api/create_user/', views.create_user, name='create_user_api'), 
      path('api/users/', views.get_users, name='get_users_api'),
 ]
